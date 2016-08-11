91d6c79968e04407a264e94a37b58e3e1c1c1cc7
Jorin Vogel <hi@jorin.me>
2016-08-11 11:37:25 +0200

    a nightmare

---
+++ nightmares/irccloud-active.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+
+// Login and logout on irccloud.com
+//
+// Prove of concept to stay connected with free account
+// if run with a scheduled job
+
+var Nightmare = require('nightmare')
+
+var email = process.env.EMAIL
+var pass = process.env.PASS
+
+if (!email || !pass) {
+  console.error('Set env vars EMAIL and PASS')
+  process.exit(1)
+}
+
+Nightmare({ show: process.env.SHOW })
+  .goto('https://irccloud.com')
+  .type('#landingLogin .form [name=email]', email)
+  .type('#landingLogin .form [name=password]', pass)
+  .click('#landingLogin .form [type=submit]')
+  .wait('#buffersContainer .memberList')
+  .click('.accountMenu__button')
+  .click('.accountMenu__signout-button')
+  .wait('#landingLogin')
+  .end()
+  .catch((error) => console.error('Login failed:', error))
