5a2faaef0bb7ff4023c3f9e33455b7bd8e440ce6
Jorin Vogel <hi@jorin.me>
2016-11-07 15:43:52 +0100

    formatting

--- follow-the-dirty-money.py
+++ follow-the-dirty-money.py
@@ -1,20 +1,21 @@
 import requests
 import re
 
+url = 'https://gist.githubusercontent.com/jorinvo/6f68380dd07e5db3cf5fd48b2465bb04/raw/c02b1e0b45ecb2e54b36e4410d0631a66d474323/fd0d929f-966f-4d1a-89cd-feee5a1c5347.json'
+visited = set()
+
 
 def get_money(url):
-    data = requests.get(url).json()
-    if data['id'] in visited:
+    transaction = requests.get(url).json()
+    if transaction['id'] in visited:
         return 0
-    match = re.search('\$[0-9,.]+', data['content'])
+    match = re.search('\$[0-9,.]+', transaction['content'])
     if not match:
         return 0
     dollar = float(match.group(0).strip('$.,').replace(',', '.'))
-    visited.add(data['id'])
-    return dollar + sum(get_money(u) for u in data['links'])
+    visited.add(transaction['id'])
+    return dollar + sum(get_money(link) for link in transaction['links'])
 
 
-url = 'https://gist.githubusercontent.com/jorinvo/6f68380dd07e5db3cf5fd48b2465bb04/raw/c02b1e0b45ecb2e54b36e4410d0631a66d474323/fd0d929f-966f-4d1a-89cd-feee5a1c5347.json'
-visited = set()
 total = get_money(url)
 print('transactions: {}, total: ${:.2f}'.format(len(visited), total))
