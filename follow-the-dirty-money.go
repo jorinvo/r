337ccc8e6a9e9d1cbdbbb8bc4b23c7ec811da166
Jorin Vogel <hi@jorin.me>
2016-11-07 15:24:35 +0100

    parallel download

--- follow-the-dirty-money.go
+++ follow-the-dirty-money.go
@@ -8,51 +8,68 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 )
+
+const workers = 10
+
+var dollarMatch = regexp.MustCompile(`\$[0-9.,]+`)
+
+type transaction struct {
+	ID      string
+	Content string
+	Links   []string
+}
 
 func main() {
 	start := "https://gist.githubusercontent.com/jorinvo/6f68380dd07e5db3cf5fd48b2465bb04/raw/c02b1e0b45ecb2e54b36e4410d0631a66d474323/fd0d929f-966f-4d1a-89cd-feee5a1c5347.json"
-	dollarMatch := regexp.MustCompile(`\$[0-9.,]+`)
 	var total float64
-	visited := make(map[string]bool)
+	visited := map[string]bool{}
 	urls := make(chan string, 1000)
-	urls <- start
+	transactions := make(chan transaction)
+	var wg sync.WaitGroup
 
-	for {
-		select {
-		case url := <-urls:
-			d := struct {
-				ID      string
-				Content string
-				Links   []string
-			}{}
-			err := getJSON(url, &d)
-			if err != nil {
-				log.Printf("failed to fetch URL '%s': %v", url, err)
-				break
+	// download in parallel
+	for w := 0; w < workers; w++ {
+		go func() {
+			for u := range urls {
+				t := transaction{}
+				err := getJSON(u, &t)
+				if err != nil {
+					log.Printf("failed to fetch URL '%s': %v", u, err)
+					wg.Done()
+					continue
+				}
+				transactions <- t
 			}
-
-			if visited[d.ID] {
-				break
-			}
-			visited[d.ID] = true
-
-			str := dollarMatch.FindString(d.Content)
-			str = strings.Trim(str, "$,.")
-			str = strings.Replace(str, ",", ".", 1)
-			dollar, err := strconv.ParseFloat(str, 64)
-			fatal(err)
-			total += dollar
-
-			for _, link := range d.Links {
-				urls <- link
-			}
-		default:
-			fmt.Printf("transactions: %d, total: $%.2f", len(visited), total)
-			close(urls)
-			return
-		}
+		}()
 	}
+
+	go func() {
+		for t := range transactions {
+			if !visited[t.ID] {
+				visited[t.ID] = true
+				s := dollarMatch.FindString(t.Content)
+				s = strings.Trim(s, "$,.")
+				s = strings.Replace(s, ",", ".", 1)
+				dollar, err := strconv.ParseFloat(s, 64)
+				fatal(err)
+				total += dollar
+				wg.Add(len(t.Links))
+				for _, link := range t.Links {
+					urls <- link
+				}
+			}
+			wg.Done()
+		}
+	}()
+
+	wg.Add(1)
+	urls <- start
+	wg.Wait()
+	close(urls)
+	close(transactions)
+	fmt.Printf("transactions: %d, total: $%.2f", len(visited), total)
 }
 
 func getJSON(url string, target interface{}) error {
