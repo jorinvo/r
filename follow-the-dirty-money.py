2d28c3a1abbd8a9aa68d3648e0d1c013b4fa8c35
Jorin Vogel <hi@jorin.me>
2016-11-10 15:17:39 +0100

    no recursion

--- follow-the-dirty-money.py
+++ follow-the-dirty-money.py
@@ -1,25 +1,24 @@
 import requests
 import re
 
-url = 'https://gist.githubusercontent.com/jorinvo/6f68380dd07e5db3cf5fd48b2465bb04/raw/c02b1e0b45ecb2e54b36e4410d0631a66d474323/fd0d929f-966f-4d1a-89cd-feee5a1c5347.json'
+start = 'https://gist.githubusercontent.com/jorinvo/6f68380dd07e5db3cf5fd48b2465bb04/raw/c02b1e0b45ecb2e54b36e4410d0631a66d474323/fd0d929f-966f-4d1a-89cd-feee5a1c5347.json'
 visited = set()
+total = 0
+urls = [start]
 
-
-def get_money(url):
+while len(urls) > 0:
     try:
-        transaction = requests.get(url).json()
+        transaction = requests.get(urls.pop()).json()
     except Exception:
         print('failed to get url')
-        return 0
+        continue
     if transaction['id'] in visited:
-        return 0
+        continue
+    visited.add(transaction['id'])
     match = re.search('\$[0-9,.]+', transaction['content'])
     if not match:
-        return 0
-    dollar = float(match.group(0).strip('$.,').replace(',', '.'))
-    visited.add(transaction['id'])
-    return dollar + sum(get_money(link) for link in transaction['links'])
+        continue
+    total += float(match.group(0).strip('$.,').replace(',', '.'))
+    urls.extend(transaction['links'])
 
-
-total = get_money(url)
 print('transactions: {}, total: ${:.2f}'.format(len(visited), total))
